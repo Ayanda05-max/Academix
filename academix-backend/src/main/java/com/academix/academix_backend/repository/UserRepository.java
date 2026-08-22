1767d312b43dc614434b2f03c870dc2bf2db87ac
Ayanda Zama <ayandazama039@gmail.com>
2026-08-22 14:22:22 +0200

    Created AuthService with regiter and login methods

--- academix-backend/src/main/java/com/academix/academix_backend/repository/UserRepository.java
+++ academix-backend/src/main/java/com/academix/academix_backend/repository/UserRepository.java
@@ -6,7 +6,7 @@ import com.academix.academix_backend.model.User;
 import java.util.List;
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
  
     public Optional<User> findByEmail(String email); //finds a user when they log in using their email.
